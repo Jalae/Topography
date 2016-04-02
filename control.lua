825c93ec49f26fa26048f093f41f888043ce2b8b
Kehnin Dyer <kehnin@gmail.com>
2016-04-02 10:58:03 +0300

    Working
    Changed images
    fixed control and entity types to properly add the objects

--- control.lua
+++ control.lua
@@ -1,65 +1,78 @@
 require "util"
 require "defines"
 
-dofile ("lib/list.lua")
-
 function drawregion(s, tl, br)
-	isostep = 300
+    isostep = 300
 
-	x = tl["x"]
-	while x < (br["x"] - 1) do
-		--game.player.print("I got here " .. x .. " " .. iso )	
-		y = tl["y"]
-		while y < (br["y"] - 1) do
-			--game.player.print("I got here " .. x .. " " .. y .. " : " .. s.get_tileproperties(x, y).elevation)
-			 c = 0
-			 
-			 e1 = s.get_tileproperties(x, y).elevation
-			 e2 = s.get_tileproperties(x+1, y).elevation
-			 e3 = s.get_tileproperties(x+1, y+1).elevation
-			 e4 = s.get_tileproperties(x, y+1).elevation
-			 
-			 list = {}
-			 v = math.floor(e1/isostep)
-			 list[v * isostep] = 1
-			 list[(v+1) * isostep] = 1
-			 v = math.floor(e2/isostep)
-			 list[v * isostep] = 1
-			 list[(v+1) * isostep] = 1
-			 v = math.floor(e3/isostep)
-			 list[v * isostep] = 1
-			 list[(v+1) * isostep] = 1
-			 v = math.floor(e4/isostep)
-			 list[v * isostep] = 1
-			 list[(v+1) * isostep] = 1
-			 
-			 for iso, _ in pairs(list) do
-				 if e1 > iso then s.create_entity{name = "topo", position = {x, y}} end
-				 if e2 > iso then s.create_entity{name = "topo", position = {x+1, y}} end
-				 if e3 > iso then s.create_entity{name = "topo", position = {x+1, y+1}} end
-				 if e4 > iso then s.create_entity{name = "topo", position = {x, y+1}} end
+    x = tl["x"]
+    count = 0
+    while x < (br["x"]) do
+        --game.player.print("I got here " .. x .. " " .. iso )  
+        y = tl["y"]
+        while y < (br["y"]) do
+            e1 = s.get_tileproperties(x, y).elevation
+            e2 = s.get_tileproperties(x+1, y).elevation
+            e3 = s.get_tileproperties(x+1, y+1).elevation
+            e4 = s.get_tileproperties(x, y+1).elevation
+            
+            list = {}
+            v = math.floor(e1/isostep)
+            list[v * isostep] = 1
+            list[(v+1) * isostep] = 1
+            v = math.floor(e2/isostep)
+            list[v * isostep] = 1
+            list[(v+1) * isostep] = 1
+            v = math.floor(e3/isostep)
+            list[v * isostep] = 1
+            list[(v+1) * isostep] = 1
+            v = math.floor(e4/isostep)
+            list[v * isostep] = 1
+            list[(v+1) * isostep] = 1
+            for iso, _ in pairs(list) do
+                count = count + 1
+                encode = 0
+                if e1 > iso then encode = encode + 8 end
+                if e2 > iso then encode = encode + 4 end
+                if e3 > iso then encode = encode + 2 end
+                if e4 > iso then encode = encode + 1 end
 
-			 end
+                if not (encode == 0 or encode == 15) then
+                    s.create_entity{name = "topo_".. tostring(encode), position = {x + 1, y + 1}}
+                end
 
-			y = y + 1
-		end
-		x = x + 1
-	end
+            end
+
+            y = y + 1
+        end
+        x = x + 1
+    end
+    --game.player.print("chunk generated ".. tostring(tl["x"]) .. "x".. tostring(tl["y"])  .. ": " .. tostring(count))
 end
 
 script.on_event(defines.events.on_chunk_generated, function(event)
-	drawregion(event.surface, event.area["left_top"], event.area["right_bottom"])
-	--drawregion(game.player.surface, {x = 0, y = 0}, {x = 100, y = 100})
+    drawregion(event.surface, event.area["left_top"], event.area["right_bottom"])
+    --drawregion(game.player.surface, {x = 0, y = 0}, {x = 100, y = 100})
 end)
 
 
 
-script.on_event(defines.events.on_tick, function(event)
-	pos = game.player.position
-	if pos then
-		game.player.print(tostring(math.floor(game.player.surface.get_tileproperties(pos["x"],pos["y"]).elevation)))
-	end
-end)
+
+-- script.on_event(defines.events.on_tick, function(event)
+--   pos = game.player.position
+--   s = game.player.surface
+--   x = math.floor(pos["x"])
+--   y = math.floor(pos["y"])
+--   if pos then
+--       if ((x%2) == 0) and ((y%2) == 0) then
+--           dir = game.player.walking_state.direction
+--           if  dir == defines.direction.north then s.create_entity{name = "topo_line_north", position = {x, y}, direction = dir } end
+--           if  dir == defines.direction.south then s.create_entity{name = "topo_line_south", position = {x,y}, direction = dir } end
+--           if  dir == defines.direction.east  then s.create_entity{name = "topo_line_east", position = {x, y}, direction = dir } end
+--           if  dir == defines.direction.west  then s.create_entity{name = "topo_line_west", position = {x, y}, direction = dir } end
+--       end
+--       game.player.print(tostring(math.floor(s.get_tileproperties(pos["x"],pos["y"]).elevation)))
+--   end
+--end)
 
 
 
--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -1,32 +1,357 @@
 data:extend({
+    --north--
     {
-        type = "noise-layer",
-        name = "topo",
-		collision_mask ={"ground-tile"},
-		icon = "__Elevation__/graphics/up-dark4.png",
-		flags = {"hidden"},
-		stack_size = 0;
-		variants = {
-			main ={
-				picture = "__Elevation__/graphics/up-dark4.png",
-				count = 21,
-				size = 8
-			},
-			inner_corner ={
-				picture = "__Elevation__/graphics/up-inner-corner.png",
-				count = 8
-			},
-			outer_corner ={
-				picture = "__Elevation__/graphics/up-outer-corner.png",
-				count = 8
-			},
-			side ={
-				picture = "__Elevation__/graphics/up-dark-side.png",
-				count = 8
-			}
-		},
+        type = "simple-entity",
+        name = "topo_3",
+        icon = "__Topography__/graphics/3.png",
+        flags = {"placeable-neutral"},
+        order = "b[decorative]-k[stone-rock]-a[big]",
+        collision_mask = {"ground-tile"},
+        max_health = 0,
+        color = {a = 0.1},
+        collision_box = {{-1.5, -1.5}, {.5, .5}},
+        selection_box = {{-1.5, -1.5}, {.5, .5}},
+        selectable_in_game = false,
+        stacksize = 0,
+        pictures =
+        {
+          {
+            filename = "__Topography__/graphics/3.png",
+            width = 64,
+            height = 64,
+          }
+        },
+        --below ores
+        render_layer = "tile-transition",
+        speed = .2
+    },
+    --south
+    {
+        type = "simple-entity",
+        name = "topo_12",
+        icon = "__Topography__/graphics/12.png",
+        flags = {"placeable-neutral"},
+        order = "b[decorative]-k[stone-rock]-a[big]",
+        collision_mask = {"ground-tile"},
+        max_health = 0,
+        color = {a = 0.1},
+        collision_box = {{-1.5, -1.5}, {.5, .5}},
+        selection_box = {{-1.5, -1.5}, {.5, .5}},
+        selectable_in_game = false,
+        stacksize = 0,
+        pictures =
+        {
+          {
+            filename = "__Topography__/graphics/12.png",
+            width = 64,
+            height = 64,
+          }
+        },
+        --below ores
+        render_layer = "tile-transition",
+        speed = .2
+    },
+    --east
+    {
+        type = "simple-entity",
+        name = "topo_9",
+        icon = "__Topography__/graphics/9.png",
+        flags = {"placeable-neutral"},
+        order = "b[decorative]-k[stone-rock]-a[big]",
+        collision_mask = {"ground-tile"},
+        max_health = 0,
+        color = {a = 0.1},
+        collision_box = {{0, 0}, {0, 0}},
+        selection_box = {{-.5, -1.5}, {1.5, 1.5}},
+        selectable_in_game = false,
+        stacksize = 0,
+        pictures =
+        {
+          {
+            filename = "__Topography__/graphics/9.png",
+            width = 64,
+            height = 64,
+          }
+        },
+        --below ores
+        render_layer = "tile-transition",
+        speed = .2
+    },
+    --west
+    {
+        type = "simple-entity",
+        name = "topo_6",
+        icon = "__Topography__/graphics/6.png",
+        flags = {"placeable-neutral"},
+        order = "b[decorative]-k[stone-rock]-a[big]",
+        collision_mask = {"ground-tile"},
+        max_health = 0,
+        color = {a = 0.1},
+        collision_box = {{0, 0}, {0, 0}},
+        selection_box = {{-.5, -1.5}, {1.5, 1.5}},
+        selectable_in_game = false,
+        stacksize = 0,
+        pictures =
+        {
+          {
+            filename = "__Topography__/graphics/6.png",
+            width = 64,
+            height = 64,
+          }
+        },
+        --below ores
+        render_layer = "tile-transition",
+        speed = .2
+    },
 
-		map_color={r=1, g=0, b=0},
-		ageing=0.0
+    {
+        type = "simple-entity",
+        name = "topo_1",
+        icon = "__Topography__/graphics/1.png",
+        flags = {"placeable-neutral"},
+        order = "b[decorative]-k[stone-rock]-a[big]",
+        collision_mask = {"ground-tile"},
+        max_health = 0,
+        color = {a = 0.1},
+        collision_box = {{0, 0}, {0, 0}},
+        selection_box = {{-.5, -1.5}, {1.5, 1.5}},
+        selectable_in_game = false,
+        stacksize = 0,
+        pictures =
+        {
+          {
+            filename = "__Topography__/graphics/1.png",
+            width = 64,
+            height = 64,
+          }
+        },
+        --below ores
+        render_layer = "tile-transition",
+        speed = .2
+    },
+    {
+        type = "simple-entity",
+        name = "topo_2",
+        icon = "__Topography__/graphics/2.png",
+        flags = {"placeable-neutral"},
+        order = "b[decorative]-k[stone-rock]-a[big]",
+        collision_mask = {"ground-tile"},
+        max_health = 0,
+        color = {a = 0.1},
+        collision_box = {{0, 0}, {0, 0}},
+        selection_box = {{-.5, -1.5}, {1.5, 1.5}},
+        selectable_in_game = false,
+        stacksize = 0,
+        pictures =
+        {
+          {
+            filename = "__Topography__/graphics/2.png",
+            width = 64,
+            height = 64,
+          }
+        },
+        --below ores
+        render_layer = "tile-transition",
+        speed = .2
+    },
+    {
+        type = "simple-entity",
+        name = "topo_4",
+        icon = "__Topography__/graphics/4.png",
+        flags = {"placeable-neutral"},
+        order = "b[decorative]-k[stone-rock]-a[big]",
+        collision_mask = {"ground-tile"},
+        max_health = 0,
+        color = {a = 0.1},
+        collision_box = {{0, 0}, {0, 0}},
+        selection_box = {{-.5, -1.5}, {1.5, 1.5}},
+        selectable_in_game = false,
+        stacksize = 0,
+        pictures =
+        {
+          {
+            filename = "__Topography__/graphics/4.png",
+            width = 64,
+            height = 64,
+          }
+        },
+        --below ores
+        render_layer = "tile-transition",
+        speed = .2
+    },
+    {
+        type = "simple-entity",
+        name = "topo_5",
+        icon = "__Topography__/graphics/5.png",
+        flags = {"placeable-neutral"},
+        order = "b[decorative]-k[stone-rock]-a[big]",
+        collision_mask = {"ground-tile"},
+        max_health = 0,
+        color = {a = 0.1},
+        collision_box = {{0, 0}, {0, 0}},
+        selection_box = {{-.5, -1.5}, {1.5, 1.5}},
+        selectable_in_game = false,
+        stacksize = 0,
+        pictures =
+        {
+          {
+            filename = "__Topography__/graphics/5.png",
+            width = 64,
+            height = 64,
+          }
+        },
+        --below ores
+        render_layer = "tile-transition",
+        speed = .2
+    },
+    {
+        type = "simple-entity",
+        name = "topo_7",
+        icon = "__Topography__/graphics/7.png",
+        flags = {"placeable-neutral"},
+        order = "b[decorative]-k[stone-rock]-a[big]",
+        collision_mask = {"ground-tile"},
+        max_health = 0,
+        color = {a = 0.1},
+        collision_box = {{0, 0}, {0, 0}},
+        selection_box = {{-.5, -1.5}, {1.5, 1.5}},
+        selectable_in_game = false,
+        stacksize = 0,
+        pictures =
+        {
+          {
+            filename = "__Topography__/graphics/7.png",
+            width = 64,
+            height = 64,
+          }
+        },
+        --below ores
+        render_layer = "tile-transition",
+        speed = .2
+    },
+    {
+        type = "simple-entity",
+        name = "topo_10",
+        icon = "__Topography__/graphics/10.png",
+        flags = {"placeable-neutral"},
+        order = "b[decorative]-k[stone-rock]-a[big]",
+        collision_mask = {"ground-tile"},
+        max_health = 0,
+        color = {a = 0.1},
+        collision_box = {{0, 0}, {0, 0}},
+        selection_box = {{-.5, -1.5}, {1.5, 1.5}},
+        selectable_in_game = false,
+        stacksize = 0,
+        pictures =
+        {
+          {
+            filename = "__Topography__/graphics/10.png",
+            width = 64,
+            height = 64,
+          }
+        },
+        --below ores
+        render_layer = "tile-transition",
+        speed = .2
+    },
+    {
+        type = "simple-entity",
+        name = "topo_11",
+        icon = "__Topography__/graphics/11.png",
+        flags = {"placeable-neutral"},
+        order = "b[decorative]-k[stone-rock]-a[big]",
+        collision_mask = {"ground-tile"},
+        max_health = 0,
+        color = {a = 0.1},
+        collision_box = {{0, 0}, {0, 0}},
+        selection_box = {{-.5, -1.5}, {1.5, 1.5}},
+        selectable_in_game = false,
+        stacksize = 0,
+        pictures =
+        {
+          {
+            filename = "__Topography__/graphics/11.png",
+            width = 64,
+            height = 64,
+          }
+        },
+        --below ores
+        render_layer = "tile-transition",
+        speed = .2
+    },
+    {
+        type = "simple-entity",
+        name = "topo_13",
+        icon = "__Topography__/graphics/13.png",
+        flags = {"placeable-neutral"},
+        order = "b[decorative]-k[stone-rock]-a[big]",
+        collision_mask = {"ground-tile"},
+        max_health = 0,
+        color = {a = 0.1},
+        collision_box = {{0, 0}, {0, 0}},
+        selection_box = {{-.5, -1.5}, {1.5, 1.5}},
+        selectable_in_game = false,
+        stacksize = 0,
+        pictures =
+        {
+          {
+            filename = "__Topography__/graphics/13.png",
+            width = 64,
+            height = 64,
+          }
+        },
+        --below ores
+        render_layer = "tile-transition",
+        speed = .2
+    },
+    {
+        type = "simple-entity",
+        name = "topo_14",
+        icon = "__Topography__/graphics/14.png",
+        flags = {"placeable-neutral"},
+        order = "b[decorative]-k[stone-rock]-a[big]",
+        collision_mask = {"ground-tile"},
+        max_health = 0,
+        color = {a = 0.1},
+        collision_box = {{0, 0}, {0, 0}},
+        selection_box = {{-.5, -1.5}, {1.5, 1.5}},
+        selectable_in_game = false,
+        stacksize = 0,
+        pictures =
+        {
+          {
+            filename = "__Topography__/graphics/14.png",
+            width = 64,
+            height = 64,
+          }
+        },
+        --below ores
+        render_layer = "tile-transition",
+        speed = .2
+    },
+    {
+        type = "simple-entity",
+        name = "topo_8",
+        icon = "__Topography__/graphics/8.png",
+        flags = {"placeable-neutral"},
+        order = "b[decorative]-k[stone-rock]-a[big]",
+        collision_mask = {"ground-tile"},
+        max_health = 0,
+        color = {a = 0.1},
+        collision_box = {{0, 0}, {0, 0}},
+        selection_box = {{-.5, -1.5}, {1.5, 1.5}},
+        selectable_in_game = false,
+        stacksize = 0,
+        pictures =
+        {
+          {
+            filename = "__Topography__/graphics/8.png",
+            width = 64,
+            height = 64,
+          }
+        },
+        --below ores
+        render_layer = "tile-transition",
+        speed = .2
     }
 })
